9c4ff71822cafaeefb69ad2f62bc27bf9b2ed749
Alan <alan@pop-os.localdomain>
2021-12-02 15:17:28 -0300

    Versão final do exercício 08

--- Exercícios/Ex08/alanhenrique08.pl
+++ Exercícios/Ex08/alanhenrique08.pl
@@ -19,37 +19,30 @@ open(FASTQ, ">$fastq_output") || die "Há um problema com o local do arquivo $fa
 
 # Checar se a sequência da enzima de restrição é válida
 
-if ($restseq !~ m/[ACTGURYSWKMBDHVN]{1,}/) {
-  die "A sequência fornecida não é válida. Use letras maiúsculas, sem espaços e caracteres especiais.\n\n";
-}
+if ($restseq !~ m/^[ACTGURYSWKMBDHVN]*$/) {die "A sequência fornecida não é válida. Use letras maiúsculas, sem espaços e caracteres especiais.\n\n"}
 
 # IF STATEMENT PARA INDEX DO $ARGV
 
-if ($#ARGV != 3) {
-  die "Usage: alanhenrique08.pl input.fasta input.qual output.fastq enzyme_sequence";
-}
+if ($#ARGV != 3) {die "Usage: alanhenrique08.pl input.fasta input.qual output.fastq enzyme_sequence"}
 
+# A seguir, uma série de REGEX para substituir possíveis letras para seus respectivos possíveis nucleotídeos (de acordo com a tabela IUPAC)
 
-# A seguir, uma série de REGEX para substituir possíveis letras para seus possíveis nucleotídeos
+$restseq =~ s/R/\[AG\]/g;
+$restseq =~ s/Y/\[CT\]/g;
+$restseq =~ s/S/\[GC\]/g;
+$restseq =~ s/W/\[AT\]/g;
+$restseq =~ s/K/\[GT\]/g;
+$restseq =~ s/M/\[AC\]/g;
+$restseq =~ s/B/\[CGT\]/g;
+$restseq =~ s/D/\[AGT\]/g;
+$restseq =~ s/H/\[ACT\]/g;
+$restseq =~ s/V/\[ACG\]/g;
+$restseq =~ s/N/\[ACTG\]/g;
 
-$restseq =~ s/R/\[AG\]/;
-$restseq =~ s/Y/\[CT\]/;
-$restseq =~ s/S/\[GC\]/;
-$restseq =~ s/W/\[AT\]/;
-$restseq =~ s/K/\[GT\]/;
-$restseq =~ s/M/\[AC\]/;
-$restseq =~ s/B/\[CGT\]/;
-$restseq =~ s/D/\[AGT\]/;
-$restseq =~ s/H/\[ACT\]/;
-$restseq =~ s/V/\[ACG\]/;
-$restseq =~ s/N/\[ACTG\]/;
-
-fastaqual();
-#print join("\n", %seq);
-
-#renzsearch();
-#fastqmk();
-
+## Rodando as três subrotinas em sequência
+fastaqual(); #Armazena sequências e qualidades em dois hashes, %seq e %qual
+renzsearch(); #Busca pelo sítio de restrição nos elementos de %seq e armazena sua posição inicial em um novo hash, %restseq
+fastqmk(); #Gera o arquivo .fastq, adicionando à descrição (linhas 1 e 3, após o nome do gene) a posição inicial dos sítios de restrição
 
 exit;
 
@@ -59,15 +52,15 @@ sub fastaqual {
   my $j = -1;
 
   while (<FASTASEQ>) { #itera por todas as linhas de FASTASEQ
-    if ($_ =~ m/name=(\w{1,})/) { #se a linha contém >
-      print "$i\n";
+    if ($_ =~ m/>/) { #se a linha contém >
       if ($i != -1) { #se i é diferente de -1, pulando assim a primeira linha
         $seq{$header} = $full_seq; #o elemento da chave header é a sequencia
         $full_seq = ""; #transforma a sequência em um string vazio
 
       }
       $i += 1; #adiciona 1 ao i
-      $header = $1; #torna o header a linha atual que contém >
+      $header = $_; #torna o header a linha atual que contém >
+      chomp $header;
     }
     unless($_ =~ m/>/) { #a menos que a linha contenha >
       $seq0 = $_; #transforma a variável seq na linha
@@ -78,15 +71,16 @@ sub fastaqual {
 
   $seq{$header} = $full_seq;
 
-  while (<QUAL>) { #itera por todas as linhas de QUAL
-    if ($_ =~ m/name=(\w{1,})/) { #se a linha contém >
+  while (<FASTAQUAL>) { #itera por todas as linhas de QUAL
+    if ($_ =~ m/>/) { #se a linha contém >
       if ($j != -1) { #se i é diferente de -1, pulando assim a primeira linha
         $qual{$header} = $full_qual; #o elemento da chave header é a sequencia
         $full_qual = ""; #transforma a sequência em um string vazio
       }
       $j += 1; #adiciona 1 ao i
-      $header = $1; #torna o header a linha atual que contém >
-      chop ($header);
+      $header = $_; #torna o header a linha atual que contém >
+      chomp $header;
+      chop $header;
     }
     unless($_ =~ m/>/) { #a menos que a linha contenha >
       $qual0 = $_; #transforma a variável seq na linha
@@ -96,40 +90,28 @@ sub fastaqual {
   #    print $full_seq;
     }
   }
-  $qual{$header} = $$full_qual;
+  $qual{$header} = $full_qual;
 
 }
 
 # SUBROTINA 2, que busca nas sequências os sítios de restrição da enzima fornecida
-# O código ficaria bem melhor se, ao invés de olhar novamente no FASTASEQ, eu olhasse no hash %seq
+## PS: No exercício é pedido o nome das sequências. No entanto, tendo usado o arquivo Chom-tr.fasta, que não inclui o mesmo "name" que aparecem em dmel-gene-r5-45.fasta,
+## salvei o cabeçalho como sendo o nome das sequências (não tenho o arquivo de qualidades do dmel). No entanto, seria fácil reverter para trabalhar com cabeçalhos mais
+## completos: seria necessário substituir o regex por m/name=(\w{1,})/ e usar $1 como o nome.
 
 sub renzsearch {
-  my $i = -1;
-  while (<FASTASEQ>) {
-    if ($_ =~ m/name=(\w{1,})/) { # if para pegar o cabeçalho do gene
-      if ($_ != -1) {
-        if ($full_seq =~ m/$restseq/) {
-          $restpos{$name} = $-[0];
-        }
-      }
-      else {
-        $i += 1;
-      }
-    $name = $1;
+  foreach $key (keys %seq) {
+    $sequence = $seq{$key};
+    if ($sequence =~ m/$restseq/) {
+      $restpos{$key} = $-[0];
     }
-    unless ($_ =~ m/^>/) {
-      $seq0 = $_;
-      chomp $seq0;
-      $full_seq = $full_seq.$seq0;
-    }
-
-  }
-  if ($full_seq =~ m/$restseq/) {
-    $restpos{$name} = $-[0];
   }
 }
 
 # SUBROTINA 3, que cria o arquivo FASTQ a partir dos dados
+## PS: como não estou familiarizados com arquivos do tipo .fastq, fiz uma busca na internet e encontrei que as linhas 1 e 3 são de descrição, sendo que a 1
+## leva o nome do gene e a 3 também, mas esta última é opcional. Desse modo, adicionei a posição inicial da sequência ao lado do nome nas linhas 1 e 3 
+## para cada gene.
 
 sub fastqmk {
   # Dicionário das qualidades para ASCII
@@ -189,9 +171,9 @@ foreach $key (keys %restpos) {
   $key_qual = $qual{$key} ." ";
   $key_fastq =~ s/>//;
 
-  print FASTQ "\@$key_fastq $\n"; ##ADICIONAR A DESCRIÇÃO PROPOSTA
+  print FASTQ "\@$key_fastq $restpos{$key}\n";
   print FASTQ "$sequence\n";
-  print FASTQ "+$key_fastq\n";
+  print FASTQ "+$key_fastq $restpos{$key}\n";
   print FASTQ "$qual{$key}\n";
 }
 
